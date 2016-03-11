13d06fdb8a1d619efd2781e39d17736b5d5cd857
Andrew Gordon <abgordon@colorado.edu>
2016-03-10 17:09:34 -0700

    comment

--- tree_gen.py
+++ tree_gen.py
@@ -3,6 +3,11 @@ from ete3 import Tree
 import ete3 #for good error messages
 import six.moves.cPickle as pickle
 
+'''
+	python 2. 7 program
+	input your tree on command line in double quotes!
+'''
+
 try:
 	newick_trees = sys.argv[1:]
 except:
